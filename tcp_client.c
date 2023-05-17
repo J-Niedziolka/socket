ddbc763feb296e99313b618f0a6310aec17c5698
jannie <jn85638@stud.uph.edu.pl>
2023-05-17 23:58:49 +0200

    two way con, to fix

--- tcp_client.c
+++ tcp_client.c
@@ -6,20 +6,34 @@
 
 #include <netinet/in.h>
 
+#include <unistd.h>
+
 int main() {
+  printf("init");
   int network_socket;
   network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+  printf("linia 13");
   //address for the socket: address family, port, 
   struct sockaddr_in server_address;
   server_address.sin_family = AF_INET6;
   server_address.sin_port = htons(9002);
   server_address.sin_addr.s_addr = INADDR_ANY;
-  
+ 
+  printf("linia 21");
   int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof server_address);
   if (connection_status == -1){
-    printf("Error while making a connectionto the socket");
+    printf("Error while making a connectionto the socket \n\n");
   }
+  
+  //receive data
+  char server_response[255];
+  recv(network_socket, &server_response, sizeof(server_response), 0);
+
+  printf("Received data: %s", server_response);
+
+  close(network_socket);
   return 0;
+
 
 }
